207984b8b26a3a66c17cad76f38813ec6a3a74f1
smithhe <smithhe@purdue.edu>
2016-09-27 16:46:51 -0400

    Imported ServerConnection to solution and made a gitignore file to prevent unecesary files from being uploaded

--- client/ServerConnection.cs
+++ client/ServerConnection.cs
@@ -1,7 +1,11 @@
-﻿namespace ATM
-{
-	public class ServerConnection
-	{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM {
+	public class ServerConnection {
 		
 	}
 }
