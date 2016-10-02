66cd3d292337b77b5b8b83a73d4ca468d74c6f90
raokrutarth <krk91@outlook.com>
2016-10-01 20:27:15 -0400

    utility classes added

---
+++ server/DBCommunicator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmServer
+{
+    class DBCommunicator
+    {
+        enum RequestType { Query, Insert, Peek};
+        public DBCommunicator()
+        {
+            /// Initilize communicator
+            /// check connection with database
+            /// check validity of  test data
+            /// setup encryption
+            /// return true on sucess
+            /// 
+        }
+        ~DBCommunicator()
+        {
+            Console.WriteLine("DBCommunicator object terminated");
+        }
+    }
+}
--- server/ServerController.cs
+++ server/ServerController.cs
@@ -9,31 +9,24 @@ namespace AtmServer
 {
     class ServerController
     {
+        public ServerController()
+        {
+
+        }
+        void serveClient()
+        {
+            /// begin handshake protocol
+            /// assign new TCP communicator object
+            /// assign new DBcomm object
+            /// terminate connection when needed
+        }
+        void recoverSession()
+        {
+
+        }
         static void Main(string[] args)
         {
-            int i = 9;
-            int loop = 15;
-            while(loop > 0)
-            {
-                Console.WriteLine("Testing...");
-                Thread nt = new Thread(() => printFib(i++) );
-                nt.Start();
-                String line = Console.ReadLine();     
-                           
-            }
-        }
-
-        static void printFib(int n)
-        {
-            Console.WriteLine("fib(" + n + ") : " + fib(n));
-        }       
-
-        static long fib(int n)
-        {
-            if (n <= 1)
-                return 1;
-            else
-                return fib(n - 1) + fib(n - 2);
+            
         }
     }
 }
