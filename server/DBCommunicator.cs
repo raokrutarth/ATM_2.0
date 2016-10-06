ff752021df1c8013e4fd674333cd6953c9b2e3d9
raokrutarth <krk91@outlook.com>
2016-10-05 23:31:58 -0400

    test db code added

--- server/DBCommunicator.cs
+++ server/DBCommunicator.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace AtmServer
 {
     class DBCommunicator
     {
         enum RequestType { Query, Insert, Peek};
+
         public DBCommunicator()
         {
             /// Initilize communicator
@@ -17,7 +19,54 @@ namespace AtmServer
             /// setup encryption
             /// return true on sucess
             /// 
+            string connectionString = "Server=tcp:atm20customer.database.windows.net,1433;Initial Catalog=CustomerDatabase;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
         }
+        private static void CreateCommand(string queryString, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+                SqlDataReader rdr = null;
+                try
+                {
+                    connection.Open();
+
+                    // 3. Pass the connection to a command object
+                    SqlCommand cmd = new SqlCommand("select * from Customers", conn);
+
+                    //
+                    // 4. Use the connection
+                    //
+
+                    // get query results
+                    rdr = cmd.ExecuteReader();
+
+                    // print the CustomerID of each record
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine(rdr[0]);
+                    }
+                }
+                finally
+                {
+                    // close the reader
+                    if (rdr != null)
+                    {
+                        rdr.Close();
+                    }
+
+                    // 5. Close the connection
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+        }
+        
         ~DBCommunicator()
         {
             Console.WriteLine("DBCommunicator object terminated");
