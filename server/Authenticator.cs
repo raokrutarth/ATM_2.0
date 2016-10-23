ee81e370b19b12741b9180f980a3a2ab847323f6
raokrutarth <krk91@outlook.com>
2016-10-23 16:39:45 -0400

    json request body created

--- server/Authenticator.cs
+++ server/Authenticator.cs
@@ -20,7 +20,34 @@ namespace AtmServer
 
             MakeFaceRequest();
             Console.WriteLine("Hit ENTER to exit...");
-            return true;            
+            return true;
+
+            /*
+             *   JSON fields in face to face verification request body:
+        <table class="element table">
+        <thead>
+        <tr><th>Fields</th><th>Type</th><th>Description</th></tr>
+        </thead>
+        <tbody>
+        <tr><td>faceId1</td><th>String</th><td>faceId of one face, comes from <a href="/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236">Face - Detect</a>.</td></tr>
+        <tr><td>faceId2</td><th>String</th><td>faceId of another face, comes from <a href="/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236">Face - Detect</a>.<br />
+        </tbody>
+        </table>                       
+        
+        JSON fields in face to person verification request body:
+        <table class="element table">
+        <thead>
+        <tr><th>Fields</th><th>Type</th><th>Description</th></tr>
+        </thead>
+        <tbody>
+        <tr><td>faceId</td><th>String</th><td>faceId the face, comes from <a href="/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395236">Face - Detect</a>.</td></tr>
+        <tr><td>personGroupId</td><th>String</th><td>Using existing personGroupId and personId for fast loading a specified person. personGroupId is created in <a href="/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f30395244">Person Group - Create a Person Group</a>.</td></tr>
+        <tr><td>personId</td><th>String</th><td>Specify a certain person in a person group. personId is created in <a href="/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f3039523c
+        ">Person - Create a Person</a>.</td></tr>
+        
+        </tbody>
+        </table>
+             */
         }
         static async void MakeFaceRequest()
         {
