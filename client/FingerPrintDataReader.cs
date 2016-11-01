ec970dcd9ce3b8cca36f13639fb4edc23529cc31
Anthony Goeckner <agoeckne@purdue.edu>
2016-11-01 01:27:07 -0400

    Added constructor for FingerPrintDataReader, which attempts to connect to device.

--- client/FingerPrintDataReader.cs
+++ client/FingerPrintDataReader.cs
@@ -3,15 +3,52 @@
  * Takes in data from the fingerprint scanner.
  */
 
+using System;
 using ScanAPIHelper;
 
 namespace ATM
 {
 	class FingerPrintDataReader
 	{
-		public FingerPrintDataReader()
-		{
+		bool connected;
+		int interfaceNumber;
 
+		/*
+		 * Creates an interface to the fingerprint reader.
+		 * \param waitForDevice If true, will block until the device is connected.
+		 */
+		public FingerPrintDataReader(bool waitForDevice)
+		{
+			connected = false;
+			interfaceNumber = Device.BaseInterface;
+			try
+			{
+				FTRSCAN_INTERFACE_STATUS[] status = Device.GetInterfaces();
+				while (true)
+				{
+					if (status[interfaceNumber] == FTRSCAN_INTERFACE_STATUS.FTRSCAN_INTERFACE_STATUS_CONNECTED)
+					{
+						Console.WriteLine("Fingerprint reader connected.");
+						connected = true;
+					}
+					if(!waitForDevice)
+					{
+						break;
+					}
+					System.Threading.Thread.Sleep(1000);
+				}
+			}
+			catch (ScanAPIException ex)
+			{
+				Console.WriteLine("ERROR: Fingerprint reader initialization: {0}", ex.ToString());
+			}
+			finally
+			{
+				if(!connected)
+				{
+					Console.WriteLine("Fingerprint reader connection failed.");
+				}
+			}
 		}
 	}
 }
--- client/HardwareReader.cs
+++ client/HardwareReader.cs
@@ -4,20 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ATM {
-	class HardwareReader {
+namespace ATM
+{
+	class HardwareReader
+	{
 		/*Methods currently return void for compilation*/
 		private int serialPort;
-		private CameraDataReader cam = new CameraDataReader();
-		private FingerPrintDataReader fin = new FingerPrintDataReader();
-		private TouchScreenDataReader touch = new TouchScreenDataReader();
-		private ButtonReader button = new ButtonReader();
+		private CameraDataReader cameraReader;
+		private FingerPrintDataReader fingerprintReader;
+		private TouchScreenDataReader touch;
+		private ButtonReader buttonReader;
 
-		public void readData() {
+		public HardwareReader()
+		{
+			cameraReader = new CameraDataReader();
+			Console.WriteLine("start");
+			fingerprintReader = new FingerPrintDataReader(true);
+			Console.WriteLine("end");
+			touch = new TouchScreenDataReader();
+			buttonReader = new ButtonReader();
+		}
+
+		public void readData()
+		{
 			
 		}
 
-		public bool validate() {
+		public bool validate()
+		{
 			return false;
 		}
 	}
