ae0596e6f82487c69e557a5648fde8d0569f968f
smithhe <smithhe@purdue.edu>
2016-12-03 15:40:29 -0500

    Removed unused code

--- server/TCPCommunicator.cs
+++ server/TCPCommunicator.cs
@@ -57,19 +57,12 @@ namespace AtmServer {
 	}
 
     public class TCPCommunicator {
-		//private ServerController controller = new ServerController();
+		public static ManualResetEvent allDone = new ManualResetEvent(false); // Thread signal
 
-		// Thread signal.
-		public static ManualResetEvent allDone = new ManualResetEvent(false);
+		public Command currentCommand; //holds the current command data
 
-		//used for callback functions
-		//private Dictionary<string, TCPDataCallback> callbacks;
+		public Socket listener; //socket used for client connection
 		
-		//holds the current command data
-		public Command currentCommand;
-		
-		//socket used for client connection
-		public Socket listener;		
 		//constructor
 		public TCPCommunicator() {
 			this.currentCommand = new Command();
@@ -79,9 +72,7 @@ namespace AtmServer {
             // Data buffer for incoming data.
             byte[] bytes = new Byte[1024];
 
-
-			try
-			{
+			try {
 				// Establish the local endpoint for the socket.
 				// The DNS name of the computer
 				// running the listener is "host.contoso.com".
@@ -95,14 +86,11 @@ namespace AtmServer {
 				// Create a TCP/IP socket.
 				this.listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-
 				// Bind the socket to the local endpoint and listen for incoming connections.
 				listener.Bind(localEndPoint);
 				listener.Listen(100);
 
-				while (true)
-				{
-
+				while (true) {
 					// Set the event to nonsignaled state.
 					allDone.Reset();
 
@@ -125,14 +113,16 @@ namespace AtmServer {
 			}
 
             Console.WriteLine("\nPress ENTER to continue...");
-            Console.Read();
+            Console.ReadKey();
         }
 
         public void AcceptCallback(IAsyncResult ar) {
             // Signal the main thread to continue.
             allDone.Set();
-            Console.WriteLine("Setting up connection\n");
-            // Get the socket that handles the client request.
+
+			//Console.WriteLine("Setting up connection\n");
+            
+			// Get the socket that handles the client request.
             Socket listener = (Socket)ar.AsyncState;
             Socket handler = listener.EndAccept(ar);
 
@@ -145,7 +135,6 @@ namespace AtmServer {
 
         public void ReadCallback(IAsyncResult ar) {
             String content = String.Empty;
-			//Console.WriteLine("Reading Data");
 
 			try {
 
@@ -157,14 +146,11 @@ namespace AtmServer {
 				// Read data from the client socket. 
 				int bytesRead = handler.EndReceive(ar);
 
-				
-
 				if (bytesRead > 0) {
                     // There  might be more data, so store the data received so far.
                     state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
 
-                    // Check for end-of-file tag. If it is not there, read 
-                    // more data.
+                    // Check for end-of-file tag. If it is not there, read more data.
                     content = state.sb.ToString();
 
                     if (this.currentCommand.size == 0 && content.Contains("\n")) {
@@ -188,7 +174,6 @@ namespace AtmServer {
                         this.decoder(state.clientData, state.sb.ToString(), this.currentCommand.size);
                         state.sb.Clear();
 						this.currentCommand = new Command();
-
 					}
 
                     handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
