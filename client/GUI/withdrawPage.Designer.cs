5099e2c5ce5d62e941ac1093989c659ab8bdefb1
Austin Reed <reed176@purdue.edu>
2016-12-05 21:04:48 -0500

    started dep & wthdrw pages

---
+++ client/GUI/withdrawPage.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ATMClient.GUI
+{
+    partial class withdrawPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bankIcon = new System.Windows.Forms.Label();
+            this.bankName = new System.Windows.Forms.Label();
+            this.fbground = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.fbground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bankIcon
+            // 
+            this.bankIcon.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bankIcon.BackColor = System.Drawing.Color.Transparent;
+            this.bankIcon.Image = global::ATMClient.GUI.Properties.Resources.HSVC_image1;
+            this.bankIcon.Location = new System.Drawing.Point(530, 20);
+            this.bankIcon.MinimumSize = new System.Drawing.Size(110, 60);
+            this.bankIcon.Name = "bankIcon";
+            this.bankIcon.Size = new System.Drawing.Size(110, 60);
+            this.bankIcon.TabIndex = 3;
+            // 
+            // bankName
+            // 
+            this.bankName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.bankName.AutoSize = true;
+            this.bankName.BackColor = System.Drawing.Color.Transparent;
+            this.bankName.Font = new System.Drawing.Font("Rockwell", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bankName.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.bankName.Location = new System.Drawing.Point(355, 15);
+            this.bankName.Name = "bankName";
+            this.bankName.Size = new System.Drawing.Size(184, 68);
+            this.bankName.TabIndex = 2;
+            this.bankName.Text = "HSBC";
+            // 
+            // fbground
+            // 
+            this.fbground.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fbground.BackgroundImage = global::ATMClient.GUI.Properties.Resources.fbg_image;
+            this.fbground.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.fbground.Controls.Add(this.bankIcon);
+            this.fbground.Controls.Add(this.bankName);
+            this.fbground.Location = new System.Drawing.Point(0, 0);
+            this.fbground.Name = "fbground";
+            this.fbground.Size = new System.Drawing.Size(990, 115);
+            this.fbground.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "How much would you like?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(301, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(387, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(451, 488);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(763, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(613, 444);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(613, 372);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(613, 305);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(301, 444);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(301, 372);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // withdrawPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ATMClient.GUI.Properties.Resources.bbg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fbground);
+            this.Name = "withdrawPage";
+            this.Text = "withdrawPage";
+            this.fbground.ResumeLayout(false);
+            this.fbground.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label bankIcon;
+        private System.Windows.Forms.Label bankName;
+        private System.Windows.Forms.Panel fbground;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
